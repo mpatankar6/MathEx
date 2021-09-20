b8661ce4f124f8d80d22e5ddc11c96488c99e84a
student <mpatankar06@gmail.com>
2021-09-20 11:07:23 -0400

    a

---
+++ src/com/company/MathEx.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MathEx {
+
+    public int swap(int num) {
+        String numstr = Integer.toString(num);
+        numstr.substring(0, numstr.length() -2);
+
+    }
+}
